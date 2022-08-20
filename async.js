1819053850fcda9943f36a2b3d5903aa3835034e
Munna <ahmedmunna4200@gmail.com>
2022-08-20 07:01:06 +0600

    javascript programming fundamental

--- async.js
+++ async.js
@@ -23,17 +23,36 @@
 // }
 // calculator(4, 5, display);
 
-// assincronus
+// asyncronus
+
+
 // setTimeout and setIntervel
-console.log("Line 1");
-setTimeout(function() {
-    console.log("Line 2");
-}, 2000);
-console.log("Line 3");
-console.log
+// console.log("Line 1");
+// setTimeout(function() {
+//     console.log("Line 2");
+// }, 2000);
+// console.log("Line 3");
+// console.log
 
+const status = true;
 
+const promise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        if(status) {
+            resolve("Promising is successfull");
+        }else{
+            reject("Failed");
+        }
+    },2000);
+});
 
+promise
+    .then(function(value) {
+        console.log(value);
+    })
+    .catch(function(rvalue) {
+        console.log(rvalue);
+    });
 
 
 
